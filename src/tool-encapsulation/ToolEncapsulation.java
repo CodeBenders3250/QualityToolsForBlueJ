69781de8ba783781131d5f3f1e1ab22c662cb1a0
jsingle9 <jsingle9@msudenver.edu>
2019-11-20 15:26:44 -0700

    commiting first version of ToolEncapsulation.java

---
+++ src/tool-encapsulation/ToolEncapsulation.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class ToolEncapsulation here.
+ *
+ * @author Justin Singleton, Anna Krajeski, Brian Laprowse, Michael Kerl
+ * @version 0.0
+ */
+package edu.msudenver.bluejext.qualitytools;
+import bluej.extensions.*;
+import bluej.extensions.event.*;
+ 
+public class ToolEncapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public ToolEncapsulation() {
+		
+	}
+	
+	public String getToolName() {
+		return "";
+	}
+	
+	public String getToolVersion() {
+		return "";
+	}
+	
+	public boolean isConnected() {
+		return false;
+	}
+	
+	public void invokeCommand() {
+		
+	}
+	
+	
+
+}
+
