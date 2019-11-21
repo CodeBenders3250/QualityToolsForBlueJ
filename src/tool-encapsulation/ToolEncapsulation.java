f1a5c5fe1f79877ed175b1fb4b31e185e3491ff7
jsingle9 <jsingle9@msudenver.edu>
2019-11-21 09:42:04 -0700

    first edit of ToolEncapsulation.java

--- src/tool-encapsulation/ToolEncapsulation.java
+++ src/tool-encapsulation/ToolEncapsulation.java
@@ -15,23 +15,44 @@ public class ToolEncapsulation {
 		// TODO Auto-generated method stub
 
 	}
-	
+
+	/*The constructor should contain a URI as a parameter to tell
+	 *the code where to look for the tool in question.
+	 *
+	 * TODO Incorporate the URI into this constructor
+	 * */	
 	public ToolEncapsulation() {
 		
 	}
 	
+	/*getToolName() will return the name of the tool which is being
+	 *encapsulated.
+	 *
+	 * */
 	public String getToolName() {
 		return "";
 	}
 	
+	/*getToolVersion() will return the version of the tool which is being 
+	 *encapsulated. 
+	 *
+	 * */
 	public String getToolVersion() {
 		return "";
 	}
-	
+
+	/*isConnected() is meant to be a yes/no answer to the question of
+	 *whether or not the tool is connecting to the software
+	 *
+	 * */
 	public boolean isConnected() {
 		return false;
 	}
 	
+	/*invokeCommand() will be used to manipulate the tool with which 
+	 * a connection has been established
+	 *
+	 * */
 	public void invokeCommand() {
 		
 	}
